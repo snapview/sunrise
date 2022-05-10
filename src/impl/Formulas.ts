e3b94febb9b972baba81c33b3c9139a9444d4ee4
Patrick Braune <pbraune@snapview.de>
2022-05-10 11:22:23 +0200

    make helper formulas generic and accept plain values as well as Cells

--- src/impl/Formulas.ts
+++ src/impl/Formulas.ts
@@ -20,27 +20,52 @@ export function history<T extends Value<unknown>>(
 }
 
 /**
+ * check if `string`, `number` or `symbol` themselves are key
+ */
+type IsRecord<T> = string extends keyof T
+    ? true
+    : number extends keyof T
+    ? true
+    : symbol extends keyof T
+    ? true
+    : false
+
+/**
  * Accepts a field name and a cell a record and creates a new cell
  * that represents a single field of the source cell
  */
-export function field<F, K extends keyof F>(
+export function field<T extends Value<unknown>, K extends keyof UnwrapCell<T>>(
     fieldName: K,
-    fromCell: Cell<F>
-): FormulaCell<F[K]> {
-    return new FormulaCell((fromVal: F) => fromVal[fieldName], fromCell)
+    fromCell: T
+): T extends Cell<any>
+    ? FormulaCell<
+          IsRecord<UnwrapCell<T>> extends true
+              ? UnwrapCell<T>[K] | undefined
+              : UnwrapCell<T>[K]
+      >
+    : IsRecord<UnwrapCell<T>> extends true
+    ? UnwrapCell<T>[K] | undefined
+    : UnwrapCell<T>[K] {
+    return formula((fromVal) => fromVal[fieldName], fromCell)
 }
 
-export function byIndex<T>(
-    index: number,
-    source: Cell<T[]>
-): FormulaCell<T | undefined> {
-    return new FormulaCell((fromVal: T[]) => fromVal[index], source)
+export function byIndex<T extends Value<unknown[]>, U extends number>(
+    index: U,
+    source: T
+): T extends Cell<any>
+    ? FormulaCell<UnwrapCell<T>[U] | undefined>
+    : UnwrapCell<T>[U] | undefined {
+    return formula((fromVal) => fromVal[index], source)
 }
 
-export function toBool(source: Cell<unknown>): FormulaCell<boolean> {
-    return new FormulaCell((fromVal: unknown) => Boolean(fromVal), source)
+export function toBool<T extends Value<unknown>>(
+    source: T
+): T extends Cell<any> ? FormulaCell<boolean> : boolean {
+    return formula((fromVal) => Boolean(fromVal), source)
 }
 
-export function not(source: Cell<unknown>): FormulaCell<boolean> {
-    return new FormulaCell((fromVal: unknown) => !Boolean(fromVal), source)
+export function not<T extends Value<unknown>>(
+    source: T
+): T extends Cell<any> ? FormulaCell<boolean> : boolean {
+    return formula((fromVal) => !Boolean(fromVal), source)
 }
