f7b8e42ac16c7850c15dbb77bc08c3fc08cd9408
pabra <patrick+github-commit-2022@braune.xyz>
2022-06-01 22:00:13 +0200

    extend formula cell type tests

--- src/impl/FormulaCell.test-d.ts
+++ src/impl/FormulaCell.test-d.ts
@@ -18,3 +18,125 @@ import { FormulaCell, Cell, SourceCell, cell, map } from '../index'
     expectError(map((x: string) => x.toUpperCase(), numberCell))
     expectError(map((x) => x.toUpperCase(), numberCell))
 }
+
+// formula with multiple input
+{
+    // values
+    const numberCell = cell(1)
+    const formulaNumberCell = map((x) => x + 1, numberCell)
+    const justNumber = 1
+
+    const stringCell = cell('a')
+    const formulaStringCell = map((x) => x.toUpperCase(), stringCell)
+    const justString = 'a'
+
+    // functions passed to map
+    const mapFn = (
+        n1: number,
+        n2: number,
+        n3: number,
+        s1: string,
+        s2: string,
+        s3: string
+    ) =>
+        [
+            Math.round(n1) + Math.round(n2) + Math.round(n3),
+            s1.toUpperCase() + s2.toUpperCase() + s3.toUpperCase(),
+        ] as const
+    const badMapFnNumber = (
+        n1: number,
+        n2: number,
+        n3: boolean,
+        s1: string,
+        s2: string,
+        s3: string
+    ) =>
+        [
+            Math.round(n1) + Math.round(n2) + (n3 ? 1 : 2),
+            s1.toUpperCase() + s2.toUpperCase() + s3.toUpperCase(),
+        ] as const
+    const badMapFnString = (
+        n1: number,
+        n2: number,
+        n3: number,
+        s1: string,
+        s2: string,
+        s3: boolean
+    ) =>
+        [
+            Math.round(n1) + Math.round(n2) + Math.round(n3),
+            s1.toUpperCase() + s2.toUpperCase() + (s3 ? 'true' : 'false'),
+        ] as const
+
+    // return values of map
+    const mixedInput = map(
+        mapFn,
+        numberCell,
+        formulaNumberCell,
+        justNumber,
+        stringCell,
+        formulaStringCell,
+        justString
+    )
+    const allFormulaInput = map(
+        mapFn,
+        formulaNumberCell,
+        formulaNumberCell,
+        formulaNumberCell,
+        formulaStringCell,
+        formulaStringCell,
+        formulaStringCell
+    )
+    const allCellInput = map(
+        mapFn,
+        numberCell,
+        numberCell,
+        numberCell,
+        stringCell,
+        stringCell,
+        stringCell
+    )
+    const allRawInput = map(
+        mapFn,
+        justNumber,
+        justNumber,
+        justNumber,
+        justString,
+        justString,
+        justString
+    )
+
+    // tests
+    expectType<FormulaCell<readonly [number, string]>>(mixedInput)
+    expectType<FormulaCell<readonly [number, string]>>(allFormulaInput)
+    expectType<FormulaCell<readonly [number, string]>>(allCellInput)
+    expectType<readonly [number, string]>(allRawInput)
+
+    expectNotType<readonly [number, string]>(mixedInput)
+    expectNotType<readonly [number, string]>(allFormulaInput)
+    expectNotType<readonly [number, string]>(allCellInput)
+    expectNotType<FormulaCell<readonly [number, string]>>(allRawInput)
+
+    expectError(
+        map(
+            badMapFnNumber,
+            numberCell,
+            formulaNumberCell,
+            justNumber,
+            stringCell,
+            formulaStringCell,
+            justString
+        )
+    )
+    expectError(
+        map(
+            badMapFnString,
+            numberCell,
+            formulaNumberCell,
+            justNumber,
+            stringCell,
+            formulaStringCell,
+            justString
+        )
+    )
+}
